a81e703ed4a18244d750e69b07db1d911558a578
Mario Quintana <pyrros2000@gmail.com>
2018-01-30 14:31:57 -0800

    update 2.0

--- app.js
+++ app.js
@@ -6,57 +6,57 @@ console.log('their name is ' + name);
 // const name = 'Mario';
 
 alert('Hello ' + name);
-const tvShow = confirm('Is Top Gear your favorite TV show?');
+const tvShow = prompt('Is Top Gear your favorite TV show?');
 console.log('likes Top Gear ' + tvShow);
 //const tvShow = 'true';
 
-if (tvShow === true) {
+if (tvShow.toLowerCase() === 'yes' && 'y') {
     alert('It\'s a great show no doubt.');
 }
 else {
     alert('That\'s too bad ' + name + ' try watching it again.');
 }
 
-const hobby = confirm('Do you consider weightlifting amazing?');
+const hobby = prompt('Do you consider weightlifting amazing?');
 console.log('enjoys weightlifting ' + hobby);
 //const hobby = 'true';
 
-if (hobby === true) {
+if (hobby.toLowerCase() === 'yes' && 'y') {
     alert('It\'s a challenging and rewarding sport.');
 }
 else {
     alert('It takes a lot of discipline to get started.');
 }
 
-const snack = confirm('Do you like Mac & Cheese?');
+const snack = prompt('Do you like Mac & Cheese?');
 console.log('likes Mac and Cheese ' + snack);
 //const snack = 'false';
 
-if (snack === true) {
-    alert('I never saw it\'s appeal.');
+if (snack.toLowerCase() === 'yes' && 'y') {
+    alert('It\'s a great meal at anytime.');
 }
 else {
-    alert('It is pretty dull food.');
+    alert('You should try Elephant\'s Mac and Cheese.');
 }
 
-const vGames = confirm('Do you still play video games?');
+const vGames = prompt('Do you still play video games?');
 console.log('likes video games ' + vGames);
 //const vGames = 'false';
 
-if (vGames === true) {
-    alert('I just outgrew them.');
+if (vGames.toLowerCase() === 'yes' && 'y') {
+    alert('Civilization 5 is fun to play.');
 }
 else {
-    alert('I prefer driving or repairing stuff.');
+    alert('I guess you can outgrow some things.');
 }
 
-const age = confirm('Are you older than 25?');
+const age = prompt('Are you older than 25?');
 console.log('older than 25 ' + age);
 //const age = 'true';
 
-if (age === true) {
-    alert('yeah I just keep getting older.');
+if (age.toLowerCase() === 'yes' && 'y') {
+    alert('Getting older is a blessing.');
 }
 else {
-    alert('I\'m no longer a young grasshopper.');
+    alert('There is much to learn young grasshoper.');
 }

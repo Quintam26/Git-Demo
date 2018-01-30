58826506d89e2b85f212f11c0e14e6846d19a311
Mario Quintana <pyrros2000@gmail.com>
2018-01-30 13:49:30 -0800

    updated questions

--- app.js
+++ app.js
@@ -5,35 +5,58 @@ const name = prompt('What\'s your name?');
 console.log('their name is ' + name);
 // const name = 'Mario';
 
-alert('Hello ' + name + '.');
-const color = prompt('What\'s your favorite color?');
-console.log('favorite color is ' + color);
-// const color = 'green';
+alert('Hello ' + name);
+const tvShow = confirm('Is Top Gear your favorite TV show?');
+console.log('likes Top Gear ' + tvShow);
+//const tvShow = 'true';
 
-alert('Not a bad choice ' + name + '!');
-const pizza = confirm('Do you like hawaiian pizza?');
-console.log('like hawaiian pizza ' + pizza);
-//const pizza = 'true';
-
-if (pizza === true) {
-    alert('Great choice ' + name + ' now let\'s dig further.');
+if (tvShow === true) {
+    alert('It\'s a great show no doubt.');
 }
 else {
-    alert('Okay ' + name + ' now that we know your dislike for hawaiian pizza let\'s continue.');
+    alert('That\'s too bad ' + name + ' try watching it again.');
 }
 
-const movie = prompt('What\'s your favorite movie?');
-console.log('favorite movie is ' + movie);
-//const movie = 'The Godfather';
+const hobby = confirm('Do you consider weightlifting amazing?');
+console.log('enjoys weightlifting ' + hobby);
+//const hobby = 'true';
 
-alert('I see you are a person of culture as well ' + name + '.  And now for the last question.');
-const driving = confirm('Do you like to drive?');
-console.log('like to drive ' + driving);
-//const driving = 'true';
-
-if (driving === true) {
-    alert('I am an avid driver and car enthusiast as well!');
+if (hobby === true) {
+    alert('It\'s a challenging and rewarding sport.');
 }
 else {
-    alert('That\'s too bad ' + name + '...');
+    alert('It takes a lot of discipline to get started.');
+}
+
+const snack = confirm('Do you like Mac & Cheese?');
+console.log('likes Mac and Cheese ' + snack);
+//const snack = 'false';
+
+if (snack === true) {
+    alert('I never saw it\'s appeal.');
+}
+else {
+    alert('It is pretty dull food.');
+}
+
+const vGames = confirm('Do you still play video games?');
+console.log('likes video games ' + vGames);
+//const vGames = 'false';
+
+if (vGames === true) {
+    alert('I just outgrew them.');
+}
+else {
+    alert('I prefer driving or repairing stuff.');
+}
+
+const age = confirm('Are you older than 25?');
+console.log('older than 25 ' + age);
+//const age = 'true';
+
+if (age === true) {
+    alert('yeah I just keep getting older.');
+}
+else {
+    alert('I\'m no longer a young grasshopper.');
 }

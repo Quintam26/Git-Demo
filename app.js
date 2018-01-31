5b4d688183cc774bfa0653839b016f455397a5d1
Mario Quintana <pyrros2000@gmail.com>
2018-01-31 12:59:33 -0800

    added more responses

--- app.js
+++ app.js
@@ -6,57 +6,72 @@ console.log('their name is ' + name);
 // const name = 'Mario';
 
 alert('Hello ' + name);
-const tvShow = prompt('Is Top Gear your favorite TV show?');
-console.log('likes Top Gear ' + tvShow);
+const tvShow = prompt('Is Top Gear my favorite TV show?');
+console.log('Do I like Top Gear ' + tvShow);
 //const tvShow = 'yes';
 
 if (tvShow.toLowerCase() === 'yes' || tvShow.toLowerCase() === 'y') {
-    alert('It\'s a great show.');
+    alert('Correct!');
 }
 else if (tvShow.toLowerCase() === 'no' || tvShow.toLowerCase() === 'n') {
-    alert('That\'s too bad ' + name + ' try watching it again.');
+    alert('You should know me better by now.');
+}
+else {
+    alert('Try again.');
 }
 
-const hobby = prompt('Do you consider weightlifting amazing?');
-console.log('enjoys weightlifting ' + hobby);
+const hobby = prompt('Do I do weightlifting?');
+console.log('Do I enjoy weightlifting ' + hobby);
 //const hobby = 'yes';
 
 if (hobby.toLowerCase() === 'yes' || hobby.toLowerCase() === 'y') {
     alert('It\'s a challenging and rewarding sport.');
 }
 else if (hobby.toLowerCase() === 'no' || hobby.toLowerCase() === 'n'){
-    alert('It takes a lot of discipline to get started.');
+    alert('You got it wrong.');
+}
+else {
+    alert('Try again.');
 }
 
 const snack = prompt('Do you like Mac & Cheese?');
-console.log('likes Mac and Cheese ' + snack);
+console.log('Do I like Mac and Cheese ' + snack);
 //const snack = 'no';
 
 if (snack.toLowerCase() === 'yes' || snack.toLowerCase() === 'y') {
-    alert('It\'s a great meal anytime.');
+    alert(' I have never seen their  appeal.');
 }
 else if (snack.toLowerCase() === 'no' || snack.toLowerCase() === 'n') {
-    alert('You should try Elephant\'s Mac and Cheese.');
+    alert('Correct I prefer pizza or fries!');
+}
+else {
+    alert('Try again.');
 }
 
 const vGames = prompt('Do you still play video games?');
-console.log('likes video games ' + vGames);
+console.log('Do I play video games ' + vGames);
 //const vGames = 'yes';
 
 if (vGames.toLowerCase() === 'yes' || vGames.toLowerCase() === 'y') {
-    alert('Civilization 5 is fun to play.');
+    alert('Yep but I only play Civilization 5 now.');
 }
 else if (vGames.toLowerCase() === 'no' || vGames.toLowerCase() === 'n') {
-    alert('I guess you can outgrow some things.');
+    alert('Well it\'s not entirely like that.');
+}
+else {
+    alert('Try again.');
 }
 
 const age = prompt('Are you older than 25?');
-console.log('older than 25 ' + age);
+console.log('Am I older than 25 ' + age);
 //const age = 'yes';
 
 if (age.toLowerCase() === 'yes' || age.toLowerCase() === 'y') {
-    alert('Getting older is a blessing.');
+    alert('Getting older has done me well.');
 }
 else if (age.toLowerCase() === 'no' || age.toLowerCase() === 'n')  {
-    alert('There is much to learn young grasshoper.');
+    alert('I wish was that young!');
+}
+else {
+    alert('Try again.');
 }
